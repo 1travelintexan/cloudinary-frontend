f78f3e74d431ddda855b54421a11555981f7747e
Joshua George <horheyinc8@gmail.com>
2024-04-09 09:30:24 -0500

    first time

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import { useContext } from "react";
+import "./App.css";
+import { AuthContext } from "./contexts/AuthContext";
+import { Route, Routes } from "react-router-dom";
+import { SignUp } from "./components/SignUp";
+import { Login } from "./components/Login";
+import { HomePage } from "./components/HomePage";
+import { IsProtected } from "./components/IsProtected";
+
+function App() {
+  //this is how you grab data from the context
+  //or take food from the frig
+  const { user, setUser } = useContext(AuthContext);
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<SignUp />} />
+        <Route path="/login" element={<Login />} />
+        <Route
+          path="/home"
+          element={
+            <IsProtected>
+              <HomePage />
+            </IsProtected>
+          }
+        />
+
+        <Route path="*" element={<h1> 404 Not found</h1>} />
+      </Routes>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/SignUp.jsx
@@ -0,0 +1,84 @@
+import axios from "axios";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+export const SignUp = () => {
+  const [userName, setUserName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(null);
+
+  const nav = useNavigate();
+  //this is the onSubmit function
+  const handleSignup = async (event) => {
+    event.preventDefault();
+
+    //this is where we create the form data and add all the properties to it
+    const myFormData = new FormData();
+    const image = event.target.image.files[0];
+    myFormData.append("imageUrl", image);
+    myFormData.append("userName", userName);
+    myFormData.append("email", email);
+    myFormData.append("password", password);
+
+    // const userToCreate = { userName, email, password };
+    try {
+      const response = await axios.post(
+        "http://localhost:5005/auth/signup",
+        myFormData
+      );
+      console.log("you created a user", response.data);
+      //only if you create the new user, then you navigate to the login page
+      nav("/login");
+    } catch (err) {
+      console.log(
+        "there was an error signing up",
+        err.response.data.errorMessage
+      );
+      setError(err.response.data.errorMessage);
+    }
+  };
+  return (
+    <div>
+      <h2>Sign up with us</h2>
+      <form onSubmit={handleSignup}>
+        <label>
+          User Name:
+          <input
+            type="text"
+            value={userName}
+            onChange={(e) => {
+              setUserName(e.target.value);
+            }}
+          />
+        </label>
+        <label>
+          Email:
+          <input
+            type="email"
+            value={email}
+            onChange={(e) => {
+              setEmail(e.target.value);
+            }}
+          />
+        </label>
+        <label>
+          Password:
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
+          />
+        </label>
+        <label>
+          Profile Image:
+          <input type="file" name="image" />
+        </label>
+        <button>Sign Up</button>
+      </form>
+      {error ? <h4 className="error-message">{error}</h4> : null}
+    </div>
+  );
+};
